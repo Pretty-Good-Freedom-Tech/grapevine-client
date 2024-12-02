d1f0cec576d14c09d147bc7abfa33674a7ecfb6c
manimejia <noreply@manimejia.me>
2024-12-02 13:23:56 -0600

    UPDATE scorecard data display in demo

--- src/lib/components/ScorecardData-view.svelte
+++ src/lib/components/ScorecardData-view.svelte
@@ -8,33 +8,34 @@
 
 {#if scorecard.input}
 <div class="stats stats-horizontal w-full">  
-  <div class="stat">
+  <div class="stat place-items-center">
     <div class="stat-title">Followed By</div>
-    <div class="stat-value">{scorecard.input.count ? Math.floor(scorecard.input.count['nostr-follows']) : 0}</div>
+    <div class="stat-value">{scorecard.input.weights ? Math.floor(scorecard.input.weights) : 0}</div>
     <div class="stat-desc">users in your network</div>
   </div>  
-  <div class="stat">
-    <div class="stat-title">Degrees</div>
+  <div class="stat place-items-center">
+    <div class="stat-title">Separated By</div>
     <div class="stat-value">{scorecard.input.dos ? Math.floor(scorecard.input.dos) : 0}</div>
-    <div class="stat-desc">of separation</div>
+    <div class="stat-desc">degrees from you</div>
   </div>  
-  <div class="stat">
-    <div class="stat-title">Weight</div>
-    <div class="stat-value">{scorecard.input.weights ? Math.floor(scorecard.input.weights) : 0}</div>
-    <div class="stat-desc">of follows</div>
-  </div>  
+
 </div>
 
+<hr>
 <div class="stats stats-horizontal w-full">
-
-  <div class="stat">
-    <div class="stat-title">Muted By</div>
-    <div class="stat-value">{scorecard.input.count ? Math.floor(scorecard.input.count['nostr-mutes']) : 0}</div>
+  <div class="stat place-items-center">
+    <div class="stat-value">{scorecard.input.count['nostr-follows'] ? Math.floor(scorecard.input.count['nostr-follows']) : 0}</div>
+    <div class="stat-desc">Total Followed By</div>
   </div>  
 
-  <div class="stat">
-    <div class="stat-title">Reported By</div>
-    <div class="stat-value">{scorecard.input.count ? Math.floor(scorecard.input.count['nostr-reports']) : 0}</div>
+  <div class="stat place-items-center">
+    <div class="stat-value">{scorecard.input.count['nostr-mutes'] ? Math.floor(scorecard.input.count['nostr-mutes']) : 0}</div>
+    <div class="stat-desc">Total Muted By</div>
+  </div>  
+
+  <div class="stat place-items-center">
+    <div class="stat-value">{scorecard.input.count['nostr-reports'] ? Math.floor(scorecard.input.count['nostr-reports']) : 0}</div>
+    <div class="stat-desc">Total Reported By</div>
   </div> 
 
 </div>
